bd7f589f2b1363f43db0100d4f1d936bee6106e9
Arpy Vanyan <arpy.vanyan@gmail.com>
2025-03-19 16:35:09 +0400

    chore: add vite configs for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,11 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/masonry-grid-app/",
   plugins: [react()],
+  server: {
+    allowedHosts: ["localhost", "127.0.0.1", "arvan.github.io"],
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
