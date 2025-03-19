7c06fccfe82cea4649dc6fb12e8ed899c678cac7
Arpy Vanyan <arpy.vanyan@gmail.com>
2025-03-19 16:15:33 +0400

    feat: add zustand devtools middleware

--- src/store/usePhotoStore.ts
+++ src/store/usePhotoStore.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { devtools } from "zustand/middleware";
 import { photoBaseUrl, PER_PAGE } from "@/constants";
 import { PixelsResponse, Params, PhotoSearchRequestParams, Photo } from "@/types/photos";
 
@@ -19,52 +20,58 @@ function stringifyParams<T extends Params>(params: T) {
     .join("&");
 }
 
-export const usePhotoStore = create<PhotoStore>((set, get) => ({
-  photos: [],
-  photosById: {},
-  loading: false,
-  error: null,
-  page: 1,
-  hasNext: true,
+export const usePhotoStore = create<PhotoStore>()(
+  devtools((set, get) => ({
+    photos: [],
+    photosById: {},
+    loading: false,
+    error: null,
+    page: 1,
+    hasNext: true,
 
-  fetchPhotos: async (query?: string) => {
-    const { page, photos } = get();
-    set({ loading: true });
+    fetchPhotos: async (query?: string) => {
+      const { page, photos } = get();
+      set({ loading: true }, undefined, "photoStore/setLoading");
 
-    try {
-      const params = {
-        per_page: "" + PER_PAGE,
-        page: "" + page,
-      } as PhotoSearchRequestParams;
+      try {
+        const params = {
+          per_page: "" + PER_PAGE,
+          page: "" + page,
+        } as PhotoSearchRequestParams;
 
-      if (query) params.query = query;
+        if (query) params.query = query;
 
-      const response = await fetch(
-        `${photoBaseUrl}${query ? "search" : "curated"}?${stringifyParams(params)}`,
-        {
-          headers: { Authorization: API_KEY },
-        },
-      );
+        const response = await fetch(
+          `${photoBaseUrl}${query ? "search" : "curated"}?${stringifyParams(params)}`,
+          {
+            headers: { Authorization: API_KEY },
+          },
+        );
 
-      if (!response.ok) throw new Error("Failed to fetch photos");
+        if (!response.ok) throw new Error("Failed to fetch photos");
 
-      const data = (await response.json()) as PixelsResponse;
-      const newPhotos = [...photos];
-      data.photos.forEach((photo) => {
-        if (!newPhotos.find((p) => p.id === photo.id)) {
-          newPhotos.push(photo);
-        }
-      });
+        const data = (await response.json()) as PixelsResponse;
+        const newPhotos = [...photos];
+        data.photos.forEach((photo) => {
+          if (!newPhotos.find((p) => p.id === photo.id)) {
+            newPhotos.push(photo);
+          }
+        });
 
-      set({
-        photos: newPhotos,
-        page: page + 1, // Increment page,
-        hasNext: !!data.next_page,
-      });
-    } catch (err) {
-      set({ error: (err as Error).message });
-    } finally {
-      set({ loading: false });
-    }
-  },
-}));
+        set(
+          {
+            photos: newPhotos,
+            page: page + 1, // Increment page,
+            hasNext: !!data.next_page,
+          },
+          undefined,
+          "photoStore/setPhotos",
+        );
+      } catch (err) {
+        set({ error: (err as Error).message }, undefined, "photoStore/setError");
+      } finally {
+        set({ loading: false }, undefined, "photoStore/setLoading");
+      }
+    },
+  })),
+);
